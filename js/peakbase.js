48916de26aa3c6e6dc5fd1175fe5ea01cd076ed3
EJ <ejlgkvam@online.no>
2023-07-31 13:28:41 +0200

    Make file selector button instead of label

--- js/peakbase.js
+++ js/peakbase.js
@@ -772,3 +772,9 @@ var date = new Date();
 // Set the date
 field.value = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) + 
     '-' + date.getDate().toString().padStart(2, 0);
+
+window.onload = function() {
+  document.getElementById('upload-file-button').addEventListener('click', function() {
+    document.getElementById('file-selector').click();
+  });
+}
